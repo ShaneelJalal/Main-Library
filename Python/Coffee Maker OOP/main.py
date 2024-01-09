b41906ac11d9385a5e7d9f5ff09f48c58986f12f
Jash <73137021+ShaneelJalal@users.noreply.github.com>
2024-01-09 18:28:03 +1300

    Update main.py

--- Python/Coffee Maker OOP/main.py
+++ Python/Coffee Maker OOP/main.py
@@ -1,11 +1,13 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
 menu = Menu()
 coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
 is_on = True
-while is_on:
+
+while is_on != False:
 	options = menu.get_items()
 	choice = input(f"What would you like? ({options}): ")
 	if choice == "off":
@@ -17,3 +19,5 @@ while is_on:
 		drink = menu.find_drink(choice)
 		if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
 			coffee_maker.make_coffee(drink)
+		else:
+			is_on = False
