8cd432e867bedeade5d742431c74d2b24e9fd19f
Jash <73137021+ShaneelJalal@users.noreply.github.com>
2024-01-10 20:36:26 +1300

    Create data.py

---
+++ Python/Quiz game/data.py
@@ -0,0 +1,90 @@
+question_data = [
+		{
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Science: Computers",
+        "question": "The last Windows operating system to be based on the Windows 9x kernel was Windows 98.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    }, {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Science: Computers",
+        "question": "The first dual-core CPU was the Intel Pentium D.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    }, {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Science: Computers",
+        "question":
+        "Linus Sebastian is the creator of the Linux kernel, which went on to be used in Linux, Android, and Chrome OS.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    }, {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Science: Computers",
+        "question": "Android versions are named in alphabetical order.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    }, {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Science: Computers",
+        "question": "A Boolean value of &quot;0&quot; represents which of these words?",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    }, {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Science: Computers",
+        "question": "The HTML5 standard was published in 2014.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    }, {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Science: Computers",
+        "question": "The common software-programming acronym &quot;I18N&quot; comes from the term &quot;Interlocalization&quot;.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    }, {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Science: Computers",
+        "question": "&quot;Windows NT&quot; is a monolithic kernel.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    }, {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Science: Computers",
+        "question":
+        "Early RAM was directly seated onto the motherboard and could not be easily removed.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    }, {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Science: Computers",
+        "question":
+        "To bypass US Munitions Export Laws, the creator of the PGP published all the source code in book form. ",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    }, {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Science: Computers",
+        "question": "The first computer bug was formed by faulty wires.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    }, {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Science: Computers",
+        "question": "MacOS is based on Linux.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    }
+]
