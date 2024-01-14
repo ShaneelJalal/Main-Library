b74b778a7c12e6334a8b942a76a0b777361ff15b
Jash <73137021+ShaneelJalal@users.noreply.github.com>
2024-01-14 15:50:56 +1300

    Create paths_directories.py

---
+++ Python/File management/paths_directories.py
@@ -0,0 +1,14 @@
+# Paths and Directories
+
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+
+# path in replit. this is !absolute file path.
+# Otherwise this  is the relative file path
+
+# example: C:\Users\user\Desktop\python\project_files\my_file.txt
+with open("./project_files/my_file.txt") as file:
+    contents = file.read()
+    print(contents)
