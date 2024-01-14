e6c2aeac23baae7c2a2ad0682134ba665eb0f47e
Jash <73137021+ShaneelJalal@users.noreply.github.com>
2024-01-14 13:34:36 +1300

    Update main.py

--- Python/File management/main.py
+++ Python/File management/main.py
@@ -1,1 +1,30 @@
+"""
+my_file.txt contains base text
 
+Hello my name is Shaneel
+I am 33 years old
+My favourite food is stack of pancakes
+"""
+# FILE MANAGMENT
+
+# Open and Read a file (READ ONLY MODE)
+# file = open("my_file.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
+
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# Open and Write a file (WRITE ONLY MODE)
+# with open("my_file.txt", mode="w") as file:
+#     file.write("New text.")
+
+# Open and Append a file (APPEND ONLY MODE)
+# with open("my_file.txt", mode="a") as file:
+#     file.write("\nNew text.")
+
+# Creates a new file if it doesn't exist
+# with open("new_file.txt", mode="w") as file:
+#     file.write("New text.")
