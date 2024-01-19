c8d72a60b6638ffa17199cd2e1f62dd09ab33ab4
Jash <73137021+ShaneelJalal@users.noreply.github.com>
2024-01-19 19:31:52 +1300

    Update main.py

--- Python/Basic Python/Tkinter/main.py
+++ Python/Basic Python/Tkinter/main.py
@@ -6,8 +6,9 @@ window.minsize(width=500, height=300)
 
 # Label
 my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack(side="left")
-
+# my_label.pack(side="left")
+# my_label.place(x=100, y=200)
+my_label.grid(column=0, row=0)
 my_label["text"] = "New text"
 my_label.config(text="New Text")
 
@@ -20,12 +21,13 @@ def button_clicked():
 
 
 button = Button(text="Click Me", command= button_clicked)
-button.pack()
+# button.pack(side="left")
+button.grid(column=1, row=1)
 
 # Entry
 input = Entry(width= 10)
-input.pack()
-
+# input.pack(side="left")
+input.grid(column=2, row=2)
 # print(input.get())
 
 window.mainloop()
