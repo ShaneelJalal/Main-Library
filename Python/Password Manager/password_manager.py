0449a93d70230d29f27480b2ed6f9484274313f3
Jash <73137021+ShaneelJalal@users.noreply.github.com>
2024-01-20 16:49:00 +1300

    Update password_manager.py

--- Python/Password Manager/password_manager.py
+++ Python/Password Manager/password_manager.py
@@ -1,6 +1,5 @@
 # ---------------------------- IMPORTS -------------------------------------#
 from tkinter import *
-from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
 
