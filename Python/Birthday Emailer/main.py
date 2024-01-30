e16eba20349636682e82dcb113871858a376641d
Jash <73137021+ShaneelJalal@users.noreply.github.com>
2024-01-30 21:20:30 +1300

    Update main.py

--- Python/Birthday Emailer/main.py
+++ Python/Birthday Emailer/main.py
@@ -1,55 +1,34 @@
 '''
-STMTP 
+Birthday Mailer
 '''
 #----------------------------- IMPORTS -----------------------------#
 import smtplib
 import datetime as dt
 import random
+import pandas
 
 #----------------------------- CONSTANTS ---------------------------#
-my_email = "shaneeljalal@gmail.com"
-password = "rojx oeaq iuic csbj"
-data = "qoutes.txt"
+today = dt.datetime.now()
+today_tuple = (today.month, today.day)
+data = pandas.read_csv("birthdays.csv")
+my_email = "################"
+password = "####################"
 
-#----------------------------- MAIN --------------------------------#
+#----------------------------- MAIN -------------------------------#
 
-# SMTP
+# new_dict = {new_key:new_value for (index, row) in data.iterrows()}
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row
+                  for (index, data_row) in data.iterrows()}
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
 
-# connection = smtplib.SMTP("smtp.gmail.com")
-# connection.starttls()
-# connection.login(user=my_email, password=password)
-# connection.sendmail(from_addr=my_email,
-# 					to_addrs="shaneel.bot@gmail.com",
-# 					msg="Subject:Hello\n\nThis is the body of my email.")
-# connection.close() Dont need this line it using "with"
-
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(from_addr=my_email,
-#                         to_addrs="shaneel.bot@gmail.com",
-#                         msg="Subject:Hello\n\nThis is the body of my email.")
-
-# DATE/TIME
-# now = dt.datetime.now()
-# year = now.year
-# month = now.month
-# day_of_week = now.weekday()
-
-# date_of_birth = dt.datetime(year=1995, month=12, day=15, hour=4)
-
-now = dt.datetime.now()
-weekday = now.weekday()
-if weekday == 1: # 1 = Monday
-	with open("quotes.txt") as quote_file:
-		all_quotes = quote_file.readlines()
-		quote = random.choice(all_quotes)
-
-	print(quote)
-	with smtplib.SMTP("smtp.gmail.com") as connection:
-		connection.starttls()
-		connection.login(my_email, password)
-		connection.sendmail(
-			from_addr=my_email,
-			to_addrs=my_email,
-			msg=f"Subject:Monday Motivation\n\n{quote}")
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=birthday_person["email"],
+                            msg=f"Subject:Happy Birthday!\n\n{contents}")
